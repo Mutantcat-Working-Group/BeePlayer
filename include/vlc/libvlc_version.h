7d35668a284bdffd84c5281c63a80bcb8a132174
tyza66 <shun_@outlook.com>
2026-09-22 18:06:56 +0800

    Fix release packaging version handling

--- include/vlc/libvlc_version.h
+++ include/vlc/libvlc_version.h
@@ -39,7 +39,7 @@
 # define LIBVLC_VERSION_MINOR    (0)
 
 /** LibVLC revision */
-# define LIBVLC_VERSION_REVISION (20260923)
+# define LIBVLC_VERSION_REVISION (20260924)
 
 # define LIBVLC_VERSION_EXTRA    (0)
 
