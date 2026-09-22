e184df04c87c9e2e7e61cfe857867d1951afe1ed
tyza66 <shun_@outlook.com>
2026-09-22 18:32:40 +0800

    Fix AppImage build directory cleanup

--- include/vlc/libvlc_version.h
+++ include/vlc/libvlc_version.h
@@ -39,7 +39,7 @@
 # define LIBVLC_VERSION_MINOR    (0)
 
 /** LibVLC revision */
-# define LIBVLC_VERSION_REVISION (20260925)
+# define LIBVLC_VERSION_REVISION (20260926)
 
 # define LIBVLC_VERSION_EXTRA    (0)
 
