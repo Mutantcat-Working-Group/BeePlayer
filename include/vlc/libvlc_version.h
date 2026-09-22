a7c31358b72e02eba22dc623198e629531c67b95
tyza66 <shun_@outlook.com>
2026-09-22 18:51:14 +0800

    Fix Windows and Linux release packaging

--- include/vlc/libvlc_version.h
+++ include/vlc/libvlc_version.h
@@ -39,7 +39,7 @@
 # define LIBVLC_VERSION_MINOR    (0)
 
 /** LibVLC revision */
-# define LIBVLC_VERSION_REVISION (20260926)
+# define LIBVLC_VERSION_REVISION (20260927)
 
 # define LIBVLC_VERSION_EXTRA    (0)
 
